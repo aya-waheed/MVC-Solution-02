0befa51a634ae9b562b5f5351ffdad3a0916298b
aya waheed <ayawaheed1506@gmail.com>
2024-09-19 00:25:25 +0300

    updated

---
+++ Company.Service/Helper/EmailSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Service.Helper
+{
+    public static class EmailSettings
+    {
+
+        public static void SendEmail(Email input)
+        {
+
+            var client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential("ayawaheed905@gmail.com", "bjbcdboinsjdozqo");
+
+            client.Send("ayawaheed905@gmail.com", input.To, input.Subject, input.Body);
+
+
+        }
+
+
+    }
+}
--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -61,7 +61,7 @@ namespace Company.Web
                 options.LoginPath = "/Account/Login";
                 options.LogoutPath = "/Account/Logout";
                 options.AccessDeniedPath = "/Account/AccessDenied";
-                options.Cookie.Name = "Hamada Cookies";
+                options.Cookie.Name = "HamadaCookies";
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                 options.Cookie.SameSite = SameSiteMode.Strict;
 
@@ -90,7 +90,7 @@ namespace Company.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=SignUp}");
 
             app.Run();
         }
